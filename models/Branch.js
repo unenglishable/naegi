9a8c6d99f8f692622ec93d682492932814692fdb
unenglishable <unenglishable@gmail.com>
2017-06-17 23:54:20 -1000

    add Branch model relation to parent tree
    bookshelf

--- models/Branch.js
+++ models/Branch.js
@@ -1,9 +1,14 @@
 var path = require('path');
 var bookshelf = require(path.join(__dirname, '..', 'config', 'bookshelf'));
+var Tree = require(path.join(__dirname, 'Tree'));
 
 var Branch = Branches = bookshelf.Model.extend({
   tableName: 'branches',
   hasTimestamps: true,
+
+  tree: function() {
+    return this.belongsTo(Tree);
+  },
 
   parentBranch: function() {
     return this.belongsTo(Branch);
