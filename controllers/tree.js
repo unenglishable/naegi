25c3dd6c284b59bfba20e0acce28f4c467c437b4
unenglishable <unenglishable@gmail.com>
2017-06-17 23:53:23 -1000

    add trees controller
    has create and get all

---
+++ controllers/tree.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var Tree = Trees = require(path.join(__dirname, '..', 'models', 'Tree'));
+
+
+/**
+ * POST /
+ * create a tree
+ */
+exports.createPost = function(req, res, next) {
+  req.assert('name', 'Name cannot be blank').notEmpty();
+  req.assert('description', 'Description cannot be more than 140 characters').optional().len({ max: 140 });
+  req.assert('githubRepoLink', 'Github repo link is not a URL').isURL();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    return res.status(400).send(errors);
+  }
+
+  new Tree({
+    name: req.body.name,
+    description: req.body.email,
+    githubRepoLink: req.body.githubRepoLink
+  }).save()
+    .then(function(tree) {
+        res.send(tree);
+    });
+};
+
+/**
+ * GET /
+ *
+ * Get all trees
+ *
+ */
+exports.allGet = function(req, res, next) {
+  Trees.fetchAll()
+  .then(function(trees) {
+    res.send(trees);
+  });
+};
