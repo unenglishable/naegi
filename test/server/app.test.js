8097c6c55dd14d1bbefaae995458dd05d9298be4
unenglishable <unenglishable@gmail.com>
2017-06-19 19:16:20 -1000

    extract tree tests from app test

--- test/server/app.test.js
+++ test/server/app.test.js
@@ -1,81 +1,11 @@
 var path = require('path');
 var request = require('supertest');
 var server = require(path.join(__dirname, '..', '..', 'server'));
-var expect = require('chai').expect;
 
 describe('GET /', function() {
   it('should render ok', function(done) {
     request(server)
       .get('/')
       .expect(200, done);
-  });
-});
-
-// Trees
-describe('POST /trees', function() {
-  it('should create a tree', function() {
-    var treeOptions = {
-      username: 'Dizzney',
-      repo: 'Rattatatouille',
-      description: 'A movie about a rat, who can cook!'
-    };
-    return request(server)
-    .post('/trees')
-    .send(treeOptions)
-    .expect(200)
-    .then(response => {
-      expect(response.body).to.exist;
-      expect(response.body).to.have.all.keys([
-        'id',
-        'created_at',
-        'updated_at',
-        'username',
-        'repo',
-        'description'
-      ]);
-
-      expect(response.body.id).to.be.a('number');
-      expect(response.body.username).to.equal(treeOptions.username);
-      expect(response.body.repo).to.equal(treeOptions.repo);
-      expect(response.body.description).to.equal(treeOptions.description);
-    });
-  });
-});
-
-describe('GET /trees', function() {
-  var treeOptions = {
-    username: 'Dizzney',
-    repo: 'Rattatatouille',
-    description: 'A movie about a rat, who can cook!'
-  };
-
-  before('create a tree', function() {
-    return request(server)
-    .post('/trees')
-    .send(treeOptions)
-    .then(response => {
-      treeOptions.id = response.body.id;
-    });
-  });
-  it('should get a tree', function() {
-    return request(server)
-    .get(`/trees/${treeOptions.id}`)
-    .expect(200)
-    .then(response => {
-      expect(response.body).to.exist;
-      expect(response.body).to.have.all.keys([
-        'id',
-        'created_at',
-        'updated_at',
-        'username',
-        'repo',
-        'description'
-      ]);
-
-      expect(response.body.id).to.be.a('number');
-      expect(response.body.username).to.equal(treeOptions.username);
-      expect(response.body.repo).to.equal(treeOptions.repo);
-      expect(response.body.description).to.equal(treeOptions.description);
-    });
   });
 });
