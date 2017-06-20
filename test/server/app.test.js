ad6b430c47646ce4cd5e05ca17984c8100b8c97a
unenglishable <unenglishable@gmail.com>
2017-06-19 18:55:35 -1000

    add tree creation test

--- test/server/app.test.js
+++ test/server/app.test.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var request = require('supertest');
 var server = require(path.join(__dirname, '..', '..', 'server'));
+var expect = require('chai').expect;
 
 describe('GET /', function() {
   it('should render ok', function(done) {
@@ -10,10 +11,33 @@ describe('GET /', function() {
   });
 });
 
-describe('GET /contact', function() {
-  it('should render ok', function(done) {
-    request(server)
-      .get('/contact')
-      .expect(200, done);
+// Trees
+describe('POST /trees', function() {
+  it('should create a tree', function() {
+    var treeOptions = {
+      username: 'Dizzney',
+      repo: 'Rattatatouille',
+      description: 'A movie about a rat, who can cook!'
+    };
+    return request(server)
+    .post('/trees')
+    .send(treeOptions)
+    .expect(200)
+    .then(response => {
+      expect(response.body).to.exist;
+      expect(response.body).to.have.all.keys([
+        'id',
+        'created_at',
+        'updated_at',
+        'username',
+        'repo',
+        'description'
+      ]);
+
+      expect(response.body.id).to.be.a('number');
+      expect(response.body.username).to.equal(treeOptions.username);
+      expect(response.body.repo).to.equal(treeOptions.repo);
+      expect(response.body.description).to.equal(treeOptions.description);
+    });
   });
 });
