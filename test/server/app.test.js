4d87fef63189e6fc9da42c0b1b38fd5c12eb1d22
unenglishable <unenglishable@gmail.com>
2017-06-19 18:56:49 -1000

    add tree get test

--- test/server/app.test.js
+++ test/server/app.test.js
@@ -41,3 +41,41 @@ describe('POST /trees', function() {
     });
   });
 });
+
+describe('GET /trees', function() {
+  var treeOptions = {
+    username: 'Dizzney',
+    repo: 'Rattatatouille',
+    description: 'A movie about a rat, who can cook!'
+  };
+
+  before('create a tree', function() {
+    return request(server)
+    .post('/trees')
+    .send(treeOptions)
+    .then(response => {
+      treeOptions.id = response.body.id;
+    });
+  });
+  it('should get a tree', function() {
+    return request(server)
+    .get(`/trees/${treeOptions.id}`)
+    .expect(200)
+    .then(response => {
+      expect(response.body).to.exist;
+      expect(response.body).to.have.all.keys([
+        'id',
+        'created_at',
+        'updated_at',
+        'username',
+        'repo',
+        'description'
+      ]);
+
+      expect(response.body.id).to.be.a('number');
+      expect(response.body.username).to.equal(treeOptions.username);
+      expect(response.body.repo).to.equal(treeOptions.repo);
+      expect(response.body.description).to.equal(treeOptions.description);
+    });
+  });
+});
