2b3fb7462f0ecd5f32e1b79f07bdfc28b175ef01
unenglishable <unenglishable@gmail.com>
2017-06-19 19:04:03 -1000

    use tree.findGet in server

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.get('/branches', branchController.byTreeGet);
 // trees
 app.post('/trees', treesController.createPost);
 app.get('/trees', treesController.allGet);
+app.get('/trees/:id', treesController.findGet);
 
 app.post('/contact', contactController.contactPost);
 app.put('/account', userController.ensureAuthenticated, userController.accountPut);
